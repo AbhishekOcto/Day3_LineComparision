bfd02481446fbf07d2da88c321546258e9d0560f
Abhishek <you@example.com>
2022-07-26 14:43:52 +0530

    [Abhishek]:added comparision of two lines

--- LineComparisionUC3.java
+++ LineComparisionUC3.java
@@ -1,19 +1,47 @@
 package com.bridgelabz.lineComparision;
-import java.util.Scanner;
-public class LineComparision {
 
+import java.util.Scanner;
+
+public class LineComparisionUC3 {
+	static Scanner scan= new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparision Computation");
 		
 Line line1 = new Line();
 double l1=calculatelength(line1);
 System.out.println(l1);
+
+Line line2 = new Line(); //UC2
+double l2 = calculatelength(line2);
+System.out.println(l2);
+
+//String s1 = Double.toString(l1);
+//String s2 = Double.toString(l2);
+
+
+
+	
+	if (l1>l2){
+		System.out.println("line 1 is greater than line 2");
 	}
 	
-	//UC1
+	else if (l2>l1){
+	System.out.println("line2 is greater than line 1");
+	}
+	
+	else {
+		System.out.println("lines are equal");
+	}
+	scan.close();
+	
+	}
+
+	
+	//user inputs for line 1 and line 2
 	public static double calculatelength(Line line) {
 		
-		Scanner scan= new Scanner(System.in);
+		
 		System.out.print("enter the value of x1= ");
 		line.setX1(scan.nextDouble());
 	
@@ -25,7 +53,7 @@ System.out.println(l1);
 		
 		System.out.print("enter the value of y2= ");
 		line.setY2(scan.nextDouble());
-		scan.close();
+		
 		
 		double xCord = line.getX2()-line.getX1();
 		double xCordSquare=xCord*xCord;
@@ -35,5 +63,4 @@ System.out.println(l1);
 		System.out.println("Length of given line is = "+lengthofline);
 		return lengthofline;
 	}
-
 }
